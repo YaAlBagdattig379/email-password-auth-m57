b9156930cc516b284671ddae3d958b252b494f13
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-08 01:55:14 +0530

    from v2-m57

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import app from './firebase.init';
 const auth = getAuth(app);
 
 function App() {
-  const handleEmailChange = (event) =>{
-    console.log('something are writtenffffffffffff by me..')
+  // const handleEmailChange = (event) =>{
+  const handleEmailBlur = (event) =>{
+    console.log(event.target.value)//'something are written by me..')
+  }
+  const handlePasswordChange = (event) =>{
+    console.log(event.target.value)//'something are written by me..')
+  }
+  const formSubmit = (event) =>{
+    event.preventDefault();
+    console.log('form submit')
   }
   return (
     <div className="App">
-       <form>
-
-          <input onChange={handleEmailChange} type="email" /> <br />
-          <input type="password" />
+       <form onSubmit={formSubmit}>
+          {/* <input onChange={handleEmailChange} type="email" name="" id="" /> */}
+          <input onBlur={handleEmailBlur} type="email" name="" id="" />
+          <br />
+          <input onChange={handlePasswordChange} type="password" name="" id="" />
+          <br />
+          <input type="submit" value="" placeholder='submit' />
        </form>
     </div>
   );
