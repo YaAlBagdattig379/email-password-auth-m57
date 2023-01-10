298a3a84011b83b289f77803de0861690fe02f3b
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-11 01:39:09 +0530

    added created new user email and password v4 m57

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form'
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import app from './firebase.init';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
-  // const handleEmailChange = (event) =>{
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
   const handleEmailBlur = (event) =>{
-    console.log(event.target.value)//'something are written by me..')
+    setEmail(event.target.value)
   }
-  const handlePasswordChange = (event) =>{
-    console.log(event.target.value)//'something are written by me..')
+  const handlePasswordBlur = (event) =>{
+    setPassword(event.target.value)
   }
-  const formSubmit = (event) =>{
+  const handleFormSubmit = (event) => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result=>{
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error=>{
+      // const errorCode = error.errorCode;
+      console.error(error)
+    })
+    // console.log('form submitted !',email,password)
     event.preventDefault();
-    console.log('form submit')
+    // console.log(event.target.value)
   }
   return (
     <div>
       <div className="registration w-50 mx-auto mt-3">
       <h3 className='text-primary'>Please register!!</h3>
-      <Form onSubmit={formSubmit}>
+    <Form onSubmit={handleFormSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
@@ -34,17 +47,14 @@ function App() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control onBlur={handlePasswordChange} type="password" placeholder="Password" />
+        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
       </Form.Group>
-    
       <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
-
       </div>
     </div>
   );
 }
-
 export default App;
