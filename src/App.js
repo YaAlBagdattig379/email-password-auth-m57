ab0da20c1463e4d9f8c358e72a533eabc1220e07
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-11 17:24:41 +0530

    added v5 m57  field validation using regular expression

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [error,setError] = useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
-
+  console.log(error)
   const handleEmailBlur = (event) =>{
     setEmail(event.target.value)
   }
@@ -19,16 +21,26 @@ function App() {
     setPassword(event.target.value)
   }
   const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    if(! /(?=.*[!#$%&? "])/.test(password)){
+      setError('password should be atleat one special characters !');
+      return;
+    }
+    setValidated(true);
+      setError('');
     createUserWithEmailAndPassword(auth, email, password)
     .then(result=>{
       const user = result.user;
       console.log(user);
     })
     .catch(error=>{
-      // const errorCode = error.errorCode;
       console.error(error)
     })
-    // console.log('form submitted !',email,password)
     event.preventDefault();
     // console.log(event.target.value)
   }
@@ -36,19 +48,26 @@ function App() {
     <div>
       <div className="registration w-50 mx-auto mt-3">
       <h3 className='text-primary'>Please register!!</h3>
-    <Form onSubmit={handleFormSubmit}>
+    <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+        <Form.Control onBlur={handleEmailBlur} type="email" required    placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
+        <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+        </Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+        <Form.Control onBlur={handlePasswordBlur} type="password"  required  placeholder="Password" />
+        <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+        </Form.Control.Feedback>
       </Form.Group>
+      <p className='text-danger text-xl'>{error}</p>
       <Button variant="primary" type="submit">
         Submit
       </Button>
